fcb80b9a9076319812237b1a893d46179385cc7f
MANOJKORI <manojkori299@outllok.com>
2024-12-25 15:52:08 +0000

    added terminal config

--- init.lua
+++ init.lua
@@ -6,3 +6,29 @@ vim.opt.shiftwidth = 2
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>") TODO
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+vim.keymap.set("n", "<A-t>", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 5)
+end)
